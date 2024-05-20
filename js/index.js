17ca4d0e8f244caf92e2624115eb234f29c4147c
Erick <erick_fcsaopaulo@hotmail.com>
2024-05-20 16:31:42 -0300

    mudanca no layout finalizada

--- js/index.js
+++ js/index.js
@@ -4,11 +4,28 @@
 /* Página Home */
 document.addEventListener("DOMContentLoaded", function() {
                             
-    setTimeout(mostraNome, 300);
+    setTimeout(menor, 300);
     setInterval(pulsaScroll, 7000);
     reproduzirVideo();
 
 });
+
+function menor(){
+
+    var typed = new Typed('.menor', {
+
+        strings: ['&lt;'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(mostraNome, 50);
+
+        },
+
+    });
+
+}
 
 function mostraNome(){
 
@@ -19,7 +36,58 @@ function mostraNome(){
         cursorChar: '',
         onComplete: (self) => {
 
-            setTimeout(mostraSlogan, 500);
+            setTimeout(maior, 50);
+
+        },
+
+    });
+
+}
+
+function maior(){
+
+    var typed = new Typed('.maior', {
+
+        strings: ['/&gt;'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(mostrarVariavel, 500);
+
+        },
+
+    });
+
+}
+
+function mostrarVariavel(){
+
+    var typed = new Typed('.variavel', {
+
+        strings: ['<br>$hello_world'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(igual, 100);
+
+        },
+
+    });
+
+}
+
+function igual(){
+
+    var typed = new Typed('.igual', {
+
+        strings: [' = '],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(mostraSlogan, 100);
 
         },
 
@@ -31,12 +99,199 @@ function mostraSlogan(){
 
     var typed = new Typed('.slogan', {
 
-        strings: ['Olá, me chamo Erick Mota, tenho 27 anos e sou formado em<br>análise e desenvolvimento de sistemas.<br><br>Eu posso fazer:'],
+        strings: ['&quot;Olá, me chamo Erick Mota, tenho 27 anos e sou formado em<br>análise e desenvolvimento de sistemas.&quot;'],
         typeSpeed: 30,
         cursorChar: '',
         onComplete: (self) => {
 
-            setTimeout(mostraAtribrutos, 1000);
+            setTimeout(ponto_virgula, 100);
+
+        },
+
+    });
+
+}
+
+function ponto_virgula(){
+
+    var typed = new Typed('.ponto_virgula', {
+
+        strings: [';<br><br>'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(constt, 500);
+
+        },
+
+    });
+
+}
+
+function constt(){
+
+    var typed = new Typed('.const', {
+
+        strings: ['const'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(oqueFazer, 100);
+
+        },
+
+    });
+
+}
+
+function oqueFazer(){
+
+    var typed = new Typed('.fazer', {
+
+        strings: ['eu_posso_fazer'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(igual2, 100);
+
+        },
+
+    });
+
+}
+
+function igual2(){
+
+    var typed = new Typed('.igual2', {
+
+        strings: ['='],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(parenteses, 100);
+
+        },
+
+    });
+
+}
+
+function parenteses(){
+
+    var typed = new Typed('.parenteses', {
+
+        strings: ['()'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(seta, 100);
+
+        },
+
+    });
+
+}
+
+function seta(){
+
+    var typed = new Typed('.seta', {
+
+        strings: ['=>'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(conchete, 100);
+
+        },
+
+    });
+
+}
+
+function conchete(){
+
+    var typed = new Typed('.conchete', {
+
+        strings: ['{<br>'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(consoles, 100);
+
+        },
+
+    });
+
+}
+
+function consoles(){
+
+    var typed = new Typed('.console', {
+
+        strings: ['console'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(ponto, 100);
+
+        },
+
+    });
+
+}
+
+function ponto(){
+
+    var typed = new Typed('.ponto', {
+
+        strings: ['.'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(log, 100);
+
+        },
+
+    });
+
+}
+
+function log(){
+
+    var typed = new Typed('.log', {
+
+        strings: ['log'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(parenteses2, 100);
+
+        },
+
+    });
+
+}
+
+function parenteses2(){
+
+    var typed = new Typed('.parenteses2', {
+
+        strings: ['('],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(mostraAtribrutos, 100);
 
         },
 
@@ -48,11 +303,49 @@ function mostraAtribrutos(){
 
     var typed = new Typed('.atributos', {
 
-        strings: ['Desenvolvimento de sites institucionais', 'Desenvolvimento de sistemas web', 'Desenvolvimento de aplicativos móveis Android e IOS', 'Criação de sites responsivos', 'Programação orientada a objetos', 'Programação em PHP', 'Programação em JavaScript', 'Uso de linguagens de marcação, como HTML e CSS', 'Uso de bibliotecas front-end, como Bootstrap e Jquery', 'Modelagem de banco de dados relacional'],
+        strings: ['&quot;Desenvolvimento de sites institucionais&quot;', '&quot;Desenvolvimento de sistemas web&quot;', '&quot;Desenvolvimento de aplicativos móveis Android e IOS&quot;', '&quot;Criação de sites responsivos&quot;', '&quot;Programação orientada a objetos&quot;', '&quot;Programação em PHP&quot;', '&quot;Programação em JavaScript&quot;', '&quot;Uso de linguagens de marcação, como HTML e CSS&quot;', '&quot;Uso de bibliotecas front-end, como Bootstrap e Jquery&quot;', '&quot;Modelagem de banco de dados relacional&quot;'],
         typeSpeed: 50,
         backSpeed: 10,
         cursorChar: '|',
         loop: true,
+        onStringTyped: (array, self) => {
+
+            if(array === 0){
+
+                setTimeout(parenteses3, 100);
+
+            }
+
+        },
+
+    });
+
+}
+
+function parenteses3(){
+
+    var typed = new Typed('.parenteses3', {
+
+        strings: [')'],
+        typeSpeed: 30,
+        cursorChar: '',
+        onComplete: (self) => {
+
+            setTimeout(conchete2, 100);
+
+        },
+
+    });
+
+}
+
+function conchete2(){
+
+    var typed = new Typed('.conchete2', {
+
+        strings: ['<br>}'],
+        typeSpeed: 30,
+        cursorChar: '',
 
     });
 
